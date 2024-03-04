614e9bfbd89d2d3b7bad872d8771fc668755734b
monolok <antoinebertin@me.com>
2024-03-04 21:05:51 +0100

    fix init 0.5

--- bertinizer/__init__.py
+++ bertinizer/__init__.py
@@ -1,3 +1,4 @@
-from .plotly_classes import plot_dataset
+from .plotly_classes import plot_data, analyze_correlation, find_outliers
 
-__all__ = ['plot_dataset']
+
+__all__ = ['plot_data', 'analyze_correlation', 'find_outliers']
